8e2fbcb0349dcb34c4ff85b97abdb8a65385bfcd
haoyue <166774700+luck1520@users.noreply.github.com>
2026-06-03 10:33:29 +0800

    Add files via upload

---
+++ GAMM.R
@@ -0,0 +1,13 @@
+# Fig. 4d resilience sensitivity to MAT under different lithologies
+data <- read.csv("G:/NC/GAM_data_all_shrub.csv")
+df <- na.omit(data)
+df$lithology <- as.factor(df$lithology)
+
+m <- gamm(
+  resilience ~ s(spei) + s(awc) + s(toc) + s(st) + s(ksn) + s(hfp),
+  random = list(lithology = ~ mat),
+  data = df
+)
+
+ranef_vals <- ranef(m$lme)$lithology
+sens <- abs(ranef_vals$mat) 
